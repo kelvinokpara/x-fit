266ecf61bc52a0df9d771488b1be5bff33be1cc0
unknown <okparakelvin65@gmail.com>
2024-01-22 17:09:20 +0100

    fixed contact us link

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -4,7 +4,7 @@ const Subscribe = () => {
   return (
     <section
       className="max-coltainer flex justify-between items-center max-lg:flex-col gap-10"
-      id="contact-ue"
+      id="contact-us"
     >
       <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
         Sign Up for<span className="text-coral-red"> Updates </span>& Newsletter
